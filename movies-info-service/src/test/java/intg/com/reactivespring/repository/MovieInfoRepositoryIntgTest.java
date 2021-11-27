a1b82925c599b5c8062b85d21ab6a2f03d7cfebb
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2021-11-27 15:55:36 -0500

    s13-48 Write integration Test for findAll() MovieInfo Document

--- movies-info-service/src/test/java/intg/com/reactivespring/repository/MovieInfoRepositoryIntgTest.java
+++ movies-info-service/src/test/java/intg/com/reactivespring/repository/MovieInfoRepositoryIntgTest.java
@@ -1,10 +1,16 @@
 package com.reactivespring.repository;
 
+import com.reactivespring.domain.MovieInfo;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.test.context.ActiveProfiles;
+import reactor.test.StepVerifier;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.time.LocalDate;
+import java.util.List;
 
 @DataMongoTest
 @ActiveProfiles("test")
@@ -13,4 +19,35 @@ class MovieInfoRepositoryIntgTest {
     @Autowired
     MovieInfoRepository movieInfoRepository;
 
+    @BeforeEach
+    void setUp() {
+        var movieInfos = List.of(
+                new MovieInfo(null, "Batman Begins",
+                        2005, List.of("Christian Bale", "Michael Cane"), LocalDate.parse("2005-06-15")),
+                new MovieInfo(null, "The Dark Knight",
+                        2008, List.of("Christian Bale", "Heath Ledger"), LocalDate.parse("2008-07-18")),
+                new MovieInfo("abc", "Dark Night Rises",
+                        2012, List.of("Christian Bale", "Tom Hardy"), LocalDate.parse("2012-07-20"))
+                );
+        movieInfoRepository.saveAll(movieInfos)
+                .blockLast();
+    }
+
+    @AfterEach
+    void tearDown() {
+        movieInfoRepository.deleteAll().block();
+    }
+
+    @Test
+    void findAll() {
+        // given
+
+        // when
+        var moviesInfoFlux = movieInfoRepository.findAll().log();
+
+        // then
+        StepVerifier.create(moviesInfoFlux)
+                .expectNextCount(3)
+                .verifyComplete();
+    }
 }
