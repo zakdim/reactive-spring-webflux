1d4756183e30e8fdf187bd3ff02719eafe24fd6b
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2021-12-12 17:33:33 -0500

    s14-54 Integrating Test for the POST end point using JUnit5

---
+++ movies-info-service/src/test/java/intg/com/reactivespring/controller/MoviesInfoControllerIntgTest.java
@@ -0,0 +1,72 @@
+package com.reactivespring.controller;
+
+import com.reactivespring.domain.MovieInfo;
+import com.reactivespring.repository.MovieInfoRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+@AutoConfigureWebTestClient
+class MoviesInfoControllerIntgTest {
+
+    @Autowired
+    MovieInfoRepository movieInfoRepository;
+
+    @Autowired
+    WebTestClient webTestClient;
+
+    static final String MOVIE_INFOS_URL = "/v1/movieinfos";
+
+    @BeforeEach
+    void setUp() {
+        var movieInfos = List.of(
+                new MovieInfo(null, "Batman Begins",
+                        2005, List.of("Christian Bale", "Michael Cane"), LocalDate.parse("2005-06-15")),
+                new MovieInfo(null, "The Dark Knight",
+                        2008, List.of("Christian Bale", "Heath Ledger"), LocalDate.parse("2008-07-18")),
+                new MovieInfo("abc", "Dark Knight Rises",
+                        2012, List.of("Christian Bale", "Tom Hardy"), LocalDate.parse("2012-07-20"))
+        );
+        movieInfoRepository.saveAll(movieInfos)
+                .blockLast();
+    }
+
+    @AfterEach
+    void tearDown() {
+        movieInfoRepository.deleteAll().block();
+    }
+
+    @Test
+    void addMovieInfo() {
+        // given
+        var movieInfo = new MovieInfo(null, "Batman Begins1",
+                2005, List.of("Christian Bale", "Michael Cane"), LocalDate.parse("2005-06-15"));
+        // when
+        webTestClient
+                .post()
+                .uri(MOVIE_INFOS_URL)
+                .bodyValue(movieInfo)
+                .exchange()
+                .expectStatus()
+                .isCreated()
+                .expectBody(MovieInfo.class)
+                .consumeWith(movieInfoEntityExchangeResult -> {
+
+                    var savedMovieInfo = movieInfoEntityExchangeResult.getResponseBody();
+                    assert savedMovieInfo != null;
+                    assert savedMovieInfo.getMovieInfoId() != null;
+                });
+
+        // then
+    }
+}
