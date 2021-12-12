80dbc18b9a1bf470646bd921a144d5fb8ad39761
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2021-12-12 17:16:41 -0500

    s14-53 Build a POST endpoint to create a new MovieInfo

---
+++ movies-info-service/src/main/java/com/reactivespring/controller/MoviesInfoController.java
@@ -0,0 +1,28 @@
+package com.reactivespring.controller;
+
+import com.reactivespring.domain.MovieInfo;
+import com.reactivespring.service.MoviesInfoService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
+
+/**
+ * Created by dmitri on 2021-12-12.
+ */
+@RestController
+@RequestMapping("/v1")
+public class MoviesInfoController {
+
+    private MoviesInfoService moviesInfoService;
+
+    public MoviesInfoController(MoviesInfoService moviesInfoService) {
+        this.moviesInfoService = moviesInfoService;
+    }
+
+    @PostMapping("/movieinfos")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Mono<MovieInfo> addMovieInfo(@RequestBody MovieInfo movieInfo) {
+
+        return moviesInfoService.addMovieInfo(movieInfo).log();
+    }
+}
---
+++ movies-info-service/src/main/java/com/reactivespring/service/MoviesInfoService.java
@@ -0,0 +1,24 @@
+package com.reactivespring.service;
+
+import com.reactivespring.domain.MovieInfo;
+import com.reactivespring.repository.MovieInfoRepository;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+/**
+ * Created by dmitri on 2021-12-12.
+ */
+@Service
+public class MoviesInfoService {
+
+    private MovieInfoRepository movieInfoRepository;
+
+    public MoviesInfoService(MovieInfoRepository movieInfoRepository) {
+        this.movieInfoRepository = movieInfoRepository;
+    }
+
+    public Mono<MovieInfo> addMovieInfo(MovieInfo movieInfo) {
+
+        return movieInfoRepository.save(movieInfo);
+    }
+}
