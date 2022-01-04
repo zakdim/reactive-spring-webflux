30990a0745239799d40a676633386abe7e52ff99
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2022-01-04 12:19:10 -0500

    s15-61 Unit Test for the create MovieInfo Endpoint - POST

--- movies-info-service/src/test/java/unit/com/reactivespring/controller/MoviesInfoControllerUnitTest.java
+++ movies-info-service/src/test/java/unit/com/reactivespring/controller/MoviesInfoControllerUnitTest.java
@@ -2,7 +2,6 @@ package com.reactivespring.controller;
 
 import com.reactivespring.domain.MovieInfo;
 import com.reactivespring.service.MoviesInfoService;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
@@ -16,7 +15,9 @@ import java.time.LocalDate;
 import java.util.List;
 
 import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.isA;
 import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * Created by dmitri on 2022-01-03.
@@ -33,11 +34,10 @@ public class MoviesInfoControllerUnitTest {
 
     static final String MOVIE_INFOS_URL = "/v1/movieinfos";
 
-    private List<MovieInfo> movieInfos;
+    @Test
+    void getAllMoviesInfo() {
 
-    @BeforeEach
-    void setUp() {
-        movieInfos = List.of(
+        var movieInfos = List.of(
                 new MovieInfo(null, "Batman Begins",
                         2005, List.of("Christian Bale", "Michael Cane"), LocalDate.parse("2005-06-15")),
                 new MovieInfo(null, "The Dark Knight",
@@ -45,10 +45,6 @@ public class MoviesInfoControllerUnitTest {
                 new MovieInfo("abc", "Dark Knight Rises",
                         2012, List.of("Christian Bale", "Tom Hardy"), LocalDate.parse("2012-07-20"))
         );
-    }
-
-    @Test
-    void getAllMoviesInfo() {
 
         when(moviesInfoServiceMock.getAllMovieInfos()).thenReturn(Flux.fromIterable(movieInfos));
 
@@ -62,20 +58,14 @@ public class MoviesInfoControllerUnitTest {
                 .hasSize(3);
     }
 
-    private MovieInfo getTestMovieInfo(String movieInfoId) {
-        return movieInfos.stream()
-                .filter(movieInfo -> movieInfoId.equalsIgnoreCase(movieInfo.getMovieInfoId()))
-                .findFirst()
-                .get();
-    }
-
     @Test
     void getMovieInfoById() {
         // given
         var movieInfoId = "abc";
-        var testMovieInfo = getTestMovieInfo(movieInfoId);
+        var movieInfo = new MovieInfo("abc", "Dark Knight Rises",
+                2012, List.of("Christian Bale", "Tom Hardy"), LocalDate.parse("2012-07-20"));
 
-        when(moviesInfoServiceMock.getMovieInfoById(anyString())).thenReturn(Mono.just(testMovieInfo));
+        when(moviesInfoServiceMock.getMovieInfoById(anyString())).thenReturn(Mono.just(movieInfo));
 
         webTestClient
                 .get()
@@ -83,7 +73,40 @@ public class MoviesInfoControllerUnitTest {
                 .exchange()
                 .expectStatus()
                 .is2xxSuccessful()
+//                .expectBody(MovieInfo.class)
+//                .consumeWith(movieInfoEntityExchangeResult -> {
+//                    var movieInfo = movieInfoEntityExchangeResult.getResponseBody();
+//                    assertNotNull(movieInfo);
+//                });
                 .expectBody()
                 .jsonPath("$.name").isEqualTo("Dark Knight Rises");
+    }
+
+    @Test
+    void addMovieInfo() {
+        // given
+        var movieInfo = new MovieInfo(null, "Batman Begins1",
+                2005, List.of("Christian Bale", "Michael Cane"), LocalDate.parse("2005-06-15"));
+
+        when(moviesInfoServiceMock.addMovieInfo(isA(MovieInfo.class))).thenReturn(
+                Mono.just(new MovieInfo("mockId", "Batman Begins1",
+                        2005, List.of("Christian Bale", "Michael Cane"), LocalDate.parse("2005-06-15")))
+        );
+
+        webTestClient
+                .post()
+                .uri(MOVIE_INFOS_URL)
+                .bodyValue(movieInfo)
+                .exchange()
+                .expectStatus()
+                .isCreated()
+                .expectBody(MovieInfo.class)
+                .consumeWith(movieInfoEntityExchangeResult -> {
+
+                    var savedMovieInfo = movieInfoEntityExchangeResult.getResponseBody();
+                    assert savedMovieInfo != null;
+                    assert savedMovieInfo.getMovieInfoId() != null;
+                    assertEquals("mockId", savedMovieInfo.getMovieInfoId());
+                });
     }
 }
