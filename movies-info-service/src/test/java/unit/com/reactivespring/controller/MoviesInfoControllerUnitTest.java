a06ba5b86b25e0288d3005ed5842fe1e18f66396
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2022-01-03 20:59:48 -0500

    s15-59 Setting up the UnitTest in WebFlux

---
+++ movies-info-service/src/test/java/unit/com/reactivespring/controller/MoviesInfoControllerUnitTest.java
@@ -0,0 +1,23 @@
+package com.reactivespring.controller;
+
+import com.reactivespring.service.MoviesInfoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+/**
+ * Created by dmitri on 2022-01-03.
+ */
+@WebFluxTest(controllers = MoviesInfoController.class)
+@AutoConfigureWebTestClient
+public class MoviesInfoControllerUnitTest {
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @MockBean
+    private MoviesInfoService moviesInfoServiceMock;
+
+}
