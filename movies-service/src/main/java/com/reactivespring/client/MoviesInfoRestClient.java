d7943deede46e5941975cc93fa36be506dbf21c1
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2022-01-09 18:41:31 -0500

    s27-98 Handling 5xx in MoviesInfoService in WebClient

--- movies-service/src/main/java/com/reactivespring/client/MoviesInfoRestClient.java
+++ movies-service/src/main/java/com/reactivespring/client/MoviesInfoRestClient.java
@@ -2,6 +2,7 @@ package com.reactivespring.client;
 
 import com.reactivespring.domain.MovieInfo;
 import com.reactivespring.exception.MoviesInfoClientException;
+import com.reactivespring.exception.MoviesInfoServerException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
@@ -44,6 +45,13 @@ public class MoviesInfoRestClient {
                             .flatMap(responseMessage -> Mono.error(new MoviesInfoClientException(
                                     responseMessage, clientResponse.statusCode().value()
                             )));
+                })
+                .onStatus(HttpStatus::is5xxServerError, clientResponse -> {
+                    log.info("Status code is: {}", clientResponse.statusCode().value());
+
+                    return clientResponse.bodyToMono(String.class)
+                            .flatMap(responseMessage -> Mono.error(new MoviesInfoServerException(
+                                    "Server exception in MoviesInfoService: " + responseMessage)));
                 })
                 .bodyToMono(MovieInfo.class)
                 .log();
--- movies-service/src/main/java/com/reactivespring/globalerrorhandler/GlobalErrorHandler.java
+++ movies-service/src/main/java/com/reactivespring/globalerrorhandler/GlobalErrorHandler.java
@@ -2,6 +2,7 @@ package com.reactivespring.globalerrorhandler;
 
 import com.reactivespring.exception.MoviesInfoClientException;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -17,5 +18,11 @@ public class GlobalErrorHandler {
     public ResponseEntity<String> handleClientException(MoviesInfoClientException exception) {
         log.error("Exception caught in handleClientException: {}", exception.getMessage());
         return ResponseEntity.status(exception.getStatusCode()).body(exception.getMessage());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleRuntimeException(RuntimeException exception) {
+        log.error("Exception caught in handleRuntimeException: {}", exception.getMessage());
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(exception.getMessage());
     }
 }
