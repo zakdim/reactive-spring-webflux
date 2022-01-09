72cf38825f5b8fd544c6498c2a58ba5d0d326f46
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2022-01-09 18:24:31 -0500

    s27-97 Handling 4XX in MoviesInfoService in WebClient

--- movies-service/src/main/java/com/reactivespring/client/MoviesInfoRestClient.java
+++ movies-service/src/main/java/com/reactivespring/client/MoviesInfoRestClient.java
@@ -1,7 +1,10 @@
 package com.reactivespring.client;
 
 import com.reactivespring.domain.MovieInfo;
+import com.reactivespring.exception.MoviesInfoClientException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
@@ -10,6 +13,7 @@ import reactor.core.publisher.Mono;
  * Created by dmitri on 2022-01-09.
  */
 @Component
+@Slf4j
 public class MoviesInfoRestClient {
 
     private WebClient webClient;
@@ -28,6 +32,19 @@ public class MoviesInfoRestClient {
                 .get()
                 .uri(url, movieId)
                 .retrieve()
+                .onStatus(HttpStatus::is4xxClientError, clientResponse -> {
+                    log.info("Status code is: {}", clientResponse.statusCode().value());
+                    if (clientResponse.statusCode().equals(HttpStatus.NOT_FOUND)) {
+                        return Mono.error(new MoviesInfoClientException(
+                                "There is no MovieInfo available for the passed in ID: " + movieId,
+                                clientResponse.statusCode().value()));
+                    }
+
+                    return clientResponse.bodyToMono(String.class)
+                            .flatMap(responseMessage -> Mono.error(new MoviesInfoClientException(
+                                    responseMessage, clientResponse.statusCode().value()
+                            )));
+                })
                 .bodyToMono(MovieInfo.class)
                 .log();
     }
---
+++ movies-service/src/main/java/com/reactivespring/globalerrorhandler/GlobalErrorHandler.java
@@ -0,0 +1,21 @@
+package com.reactivespring.globalerrorhandler;
+
+import com.reactivespring.exception.MoviesInfoClientException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+/**
+ * Created by dmitri on 2022-01-09.
+ */
+@ControllerAdvice
+@Slf4j
+public class GlobalErrorHandler {
+
+    @ExceptionHandler(MoviesInfoClientException.class)
+    public ResponseEntity<String> handleClientException(MoviesInfoClientException exception) {
+        log.error("Exception caught in handleClientException: {}", exception.getMessage());
+        return ResponseEntity.status(exception.getStatusCode()).body(exception.getMessage());
+    }
+}
