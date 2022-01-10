168aa7010bd7e35df104863c6916d1367101facb
Dmitri Zakharov <dmitri.zakharov@gmail.com>
2022-01-09 22:44:31 -0500

    s30-113 Build a Streaming Endpoint in MoviesInfoService

--- movies-info-service/src/main/java/com/reactivespring/controller/MoviesInfoController.java
+++ movies-info-service/src/main/java/com/reactivespring/controller/MoviesInfoController.java
@@ -4,10 +4,12 @@ import com.reactivespring.domain.MovieInfo;
 import com.reactivespring.service.MoviesInfoService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.publisher.Sinks;
 
 import javax.validation.Valid;
 
@@ -20,6 +22,8 @@ import javax.validation.Valid;
 public class MoviesInfoController {
 
     private MoviesInfoService moviesInfoService;
+
+    Sinks.Many<MovieInfo> moviesInfoSink = Sinks.many().replay().latest();
 
     public MoviesInfoController(MoviesInfoService moviesInfoService) {
         this.moviesInfoService = moviesInfoService;
@@ -43,10 +47,17 @@ public class MoviesInfoController {
                 .log();
     }
 
+    @GetMapping(value = "/movieinfos/stream", produces = MediaType.APPLICATION_NDJSON_VALUE)
+    public Flux<MovieInfo> getMovieInfosById() {
+
+        return moviesInfoSink.asFlux();
+    }
+
     @PostMapping("/movieinfos")
     @ResponseStatus(HttpStatus.CREATED)
     public Mono<MovieInfo> addMovieInfo(@RequestBody @Valid MovieInfo movieInfo) {
-        return moviesInfoService.addMovieInfo(movieInfo).log();
+        return moviesInfoService.addMovieInfo(movieInfo)
+                .doOnNext(savedMovieInfo -> moviesInfoSink.tryEmitNext(savedMovieInfo));
 
         // public that movie to something
         // subscriber to this movie info
